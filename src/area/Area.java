61bdb410fd6fa5db9be455d10e16710ab248f507
Solomon <solomonshuga@gmail.com>
2019-08-08 13:57:08 +0100

    area of a circle

---
+++ src/area/Area.java
@@ -0,0 +1,16 @@
+
+package area;
+
+
+public class Area {
+
+   
+    public static void main(String[] args) {
+      int r = 2;
+double pi= 3.142, Area;
+
+Area = pi*r*r;
+System.out.println ("Area of circle:"+Area);
+    }
+    
+}
